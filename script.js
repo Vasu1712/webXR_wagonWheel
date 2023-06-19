42e467fb8322a66f25b4744bf82f1bbd30b423b0
Vasu Pal <vasupal.17.12.2002@gmail.com>
2023-06-19 18:59:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -121,6 +121,7 @@ function drawWagonWheels(xVal, yVal, color) {
 	console.log("heree", mesh);
 	mesh.scale.set(0.3, 0.3, 0.3);
 	mesh.position.set(0, 0, 0);
+	mesh.castShadow = true;
 	// mesh.position.set(-7, 5, -5);
 	// mesh.rotation.x = Math.PI / 7;
 	//mesh.name = "WagonWheels_" + name;
@@ -131,6 +132,7 @@ function drawWagonWheels(xVal, yVal, color) {
 	console.log(stadium);
 	stadium.add(mesh); //tubes are made children to stadium here.
 	//_runStore.push(mesh); //1,2,3,4,6 buttons, used in displaylines
+	stadium.receiveShadow = true;
 }
 
 
@@ -196,14 +198,25 @@ function init() {
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
 
-	const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-	light.position.set( 0.5, 1, 0.25 );
-	scene.add( light );
+	// const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+	// light.position.set( 0.5, 1, 0.25 );
+	// scene.add( light );
+
+	const light = new THREE.DirectionalLight(0xffffff, 1);
+	light.position.set(0, 10, 0);
+	light.castShadow = true;
+	light.shadow.mapSize.width = 1024;
+	light.shadow.mapSize.height = 1024;
+	light.shadow.bias = -0.001;
+	scene.add(light);
+
 
 	renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.xr.enabled = true;
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 	container.appendChild( renderer.domElement );
 
 	controls=new OrbitControls(camera, renderer.domElement);
